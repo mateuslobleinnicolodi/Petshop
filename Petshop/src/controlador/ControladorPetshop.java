b55fc227cd9cff8dfdc8190a436f8439f963187b
Administrador <Administrador@10.10.4.2>
2019-09-17 16:12:42 -0300

    caom tudo :)

---
+++ Petshop/src/controlador/ControladorPetshop.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+import dao.PetshopDao;
+import javax.swing.JOptionPane;
+import modelo.Petshop;
+import tela.manutencao.PetshopManutencao;
+
+import java.util.List;
+
+import java.util.Vector;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+/**
+ *
+ * @author Administrador
+ */
+public class ControladorPetshop {
+
+    public static void inserir(PetshopManutencao man){
+        Petshop objeto = new Petshop();
+        objeto.setNome(man.jtfNome.getText());
+        objeto.setEndereco(man.jtfEndereco.getText());
+        objeto.setNumero(Integer.parseInt(man.jtfNumero.getText()));
+        objeto.setAvaliacao(Integer.parseInt(man.jtfAvaliacao.getText()));
+        
+        boolean resultado = PetshopDao.inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+}
+
+    public static void alterar(PetshopManutencao man){
+        Petshop objeto = new Petshop();
+        //definir todos os atributos
+        objeto.setCodigo(Integer.parseInt(man.jtfCodigo.getText()));
+        objeto.setNome(man.jtfNome.getText());
+        objeto.setEndereco(man.jtfEndereco.getText());
+        objeto.setNumero(Integer.parseInt(man.jtfNumero.getText()));
+        objeto.setAvaliacao(Integer.parseInt(man.jtfAvaliacao.getText()));
+        
+        boolean resultado = PetshopDao.alterar(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Alterado com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+
+     public static void excluir(PetshopManutencao man){
+        Petshop objeto = new Petshop();
+        objeto.setCodigo(Integer.parseInt(man.jtfCodigo.getText())); //só precisa definir a chave primeira
+        
+        boolean resultado = PetshopDao.excluir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Excluído com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+     
+     public static void atualizarTabela(JTable tabela) {
+        DefaultTableModel modelo = new DefaultTableModel();
+        //definindo o cabeçalho da tabela
+        modelo.addColumn("Codigo");
+        modelo.addColumn("Nome");
+        modelo.addColumn("Endereço");
+        modelo.addColumn("Número");
+        modelo.addColumn("Avaliação");
+        List<Petshop> resultados = PetshopDao.consultar();
+        for (Petshop objeto : resultados) {
+            Vector linha = new Vector();
+            
+            //definindo o conteúdo da tabela
+            linha.add(objeto.getCodigo());
+            linha.add(objeto.getNome());
+            linha.add(objeto.getEndereco());
+            linha.add(objeto.getNumero());
+            linha.add(objeto.getAvaliacao());
+            modelo.addRow(linha); //adicionando a linha na tabela
+        }
+        tabela.setModel(modelo);
+    }
+}
